43600d1e1ebb068bb78e62a36940ccb9d362f6a5
tangxujia <2913223934@qq.com>
2026-05-26 11:49:06 +0800

    fix: 修复bug

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -3,7 +3,7 @@
     <van-nav-bar 
       title="我的" 
       left-text="" 
-      left-arrow="false"
+      :left-arrow="false"
     />
     
     <!-- 用户信息区域 -->
@@ -70,7 +70,7 @@
         <p>智能旅游助手 v1.0.0</p>
         <p class="mt-2">基于 AI 技术的智能旅游规划平台</p>
         <p class="mt-2">为您提供个性化的旅游行程推荐和实时旅游咨询服务</p>
-        <p class="mt-4 text-center">© 2024 智能旅游助手</p>
+        <p class="mt-4 text-center">© 2026 智能旅游助手</p>
       </div>
     </van-dialog>
   </div>
